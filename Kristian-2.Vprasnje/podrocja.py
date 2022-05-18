64217d4daf561b850c4402b391e8c4d30a928319
KristianBicba <kb3067@student.uni-lj.si>
2022-05-18 22:45:43 +0200

    2.Vprasnaje
    Test

--- Kristian-2.Vprasnje/podrocja.py
+++ Kristian-2.Vprasnje/podrocja.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-podrocja = pd.read_csv('podatki/podrocje.csv',sep=',',encoding='cp1250')
+podrocja = pd.read_csv('podatki/podrocje2.csv',sep=',',encoding='cp1250')
 #Dijaki po starosti, letnikih, spolu in vrsti izobraževanja, Slovenija, letno
 #Link: https://pxweb.stat.si/SiStatData/pxweb/sl/Data/-/0953221S.px
 moski = dict()
@@ -9,9 +9,9 @@ zenske = dict()
 #Skupaj končno število končane srednje izobrazbe
 for col in podrocja.columns:
     if ("Moški" in col):
-        moski[col] = int(podrocja[col].iloc[9])
+        moski[col] = int(podrocja[col].iloc[0])
     if ("Ženske" in col):
-        zenske[col] = int(podrocja[col].iloc[9])
+        zenske[col] = int(podrocja[col].iloc[0])
 
 
 print(moski)
